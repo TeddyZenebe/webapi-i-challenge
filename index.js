66a17cb121e59ef86fbaa8c3bfbeb1d54b0e4c6b
Your Name <tfikrie@yahoo.com>
2019-08-07 19:17:09 -0400

    all node modules and dependence are added

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // implement your API here
+const express = require('express'); 
+
+const server = express(); 
+
+// handle requests to the root of the api, the / route
+server.get('/', (req, res) => {
+  res.send('Hello from Express');
+});
+
+// watch for connections on port 5000
+server.listen(5000, () =>
+  console.log('Server running on http://localhost:5000')
+);
